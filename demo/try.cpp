77d5001b0dd325322f9ea5de806d46191ff3e57c
sukhvir-singh3 <sukhvir-singh3@myseneca.ca>
2023-02-19 14:14:18 -0500

    Initial Commit

---
+++ demo/try.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+		
+int main(){
+	int arr[9] = {2,3,4,5,6,75,75,32,10}; 	
+	return 0;
+}
--- practice_2.0/throw.cpp
+++ practice_2.0/throw.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+void check(int x){
+	if(x == 0)
+		throw "Value was Zero\n";
+	else
+		std::cout << "Not Zero"<< std::endl;
+	}
 int main(){
-
+	try{
+		check(3);
+		check(0);
+	}catch(const char* msg){
+		std::cout << msg << std::endl;
+	}
 	return 0;
 }
