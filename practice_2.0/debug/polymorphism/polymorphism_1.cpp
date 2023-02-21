b16367f55f151c7770bf910848e352c0bd8b8833
sukhvir-singh3 <sukhvir-singh3@myseneca.ca>
2023-02-20 20:40:39 -0500

    Initial commit

--- practice_2.0/debug/polymorphism/polymorphism_1.cpp
+++ practice_2.0/debug/polymorphism/polymorphism_1.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 class Animal {
 public:
+    ~Animal(){}
     virtual void makeSound() {
         cout << "Animal sound" << endl;
     }
@@ -11,6 +12,7 @@ public:
 
 class Cat : public Animal {
 public:
+    ~Cat(){}
     void makeSound() {
         cout << "Meow" << endl;
     }
@@ -18,6 +20,7 @@ public:
 
 class Dog : public Animal {
 public:
+    ~Dog(){}
     void makeSound() {
         cout << "Woof" << endl;
     }
@@ -28,8 +31,7 @@ int main() {
     Animal myAnimal = myCat;
     myAnimal.makeSound();
     
-    Animal* animalPointer;
-    animalPointer = new Animal();
+    Animal* animalPointer = new Animal();
     animalPointer->makeSound();
     
     delete animalPointer;
@@ -95,7 +97,7 @@ int main() {
 /*
 1) In the main() function, we are creating an instance of Cat and assigning it to an object of type Animal. This results in object slicing, where only the base class part of the Cat object is copied into the myAnimal object. To fix this, we should use a pointer or reference to the Animal class instead of creating an object directly, like Animal* myAnimalPointer = new Cat();.
 
-2) In the main() function, we are calling the makeSound() method on an object of the Animal class. Since makeSound() is a virtual method, we expect it to call the derived class method when called on an object of the derived class. However, since myAnimal is an object of the base Animal class, calling makeSound() on it will always call the base class method, regardless of the type of the object it refers to. To fix this, we should make myAnimal a pointer or reference to Animal class, and call the makeSound() method on it using the arrow operator ->, like Animal* myAnimalPointer = new Cat(); myAnimalPointer->makeSound();.
+2) needed a virual ~Animal(){}
 
 3) In the main() function, we allocate memory for an Animal object using the new operator, but we never free this memory using the delete operator. This results in a memory leak. To fix this, we should call delete animalPointer; to free the memory allocated for the animalPointer.
 
@@ -108,6 +110,7 @@ using namespace std;
 
 class Animal {
 public:
+    virual ~Animal(){}
     virtual void makeSound() {
         cout << "Animal sound" << endl;
     }
@@ -115,6 +118,7 @@ public:
 
 class Cat : public Animal {
 public:
+    ~Cat(){}
     void makeSound() {
         cout << "Meow" << endl;
     }
@@ -122,6 +126,7 @@ public:
 
 class Dog : public Animal {
 public:
+    ~Dog(){}
     void makeSound() {
         cout << "Woof" << endl;
     }
